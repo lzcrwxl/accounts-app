6c5588b226f695e47606ef24e1ddffd91698864e
lzcrwxl <lzcrwxl@163.com>
2018-05-02 21:26:01 +0800

    first commit

---
+++ src/components/Records.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Records extends Component {
+  render() {
+    return (
+      <div>
+        <h2></h2>  
+      </div>
+    );
+  }
+}
+
+export default Records;
